46bc8ae72cfc3c30f08d9e23beeaa4bdc30d5d2c
agkayacaner <dev.caneragkaya@gmail.com>
2022-12-18 15:01:24 +0300

    #

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ import {
 import { useState, useEffect, useRef } from 'react';
 import Cart from './Cart';
 import Searchbox from './Searchbox';
+import { FaTimes } from 'react-icons/fa';
 
 let useClickOutside = (handler) => {
   let domNode = useRef();
@@ -33,21 +34,31 @@ let useClickOutside = (handler) => {
 const Navbar = () => {
   let [isOpen, setIsOpen] = useState(false);
   const [openSearch, setOpenSearch] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
 
   let domNode = useClickOutside(() => {
     setIsOpen(false);
   });
 
+  const leftNav = [
+    { title: 'Erkek', url: 'products/1' },
+    { title: 'Kadın', url: 'products/2' },
+    { title: 'Çocuk', url: 'products/3' },
+    { title: 'Aksesuar', url: 'products/4' },
+  ];
+
   return (
     <div className='grid grid-cols-3 px-10 py-5 bg-white dark:bg-[#000]'>
-      <button className='md:hidden'>
+      <button
+        className='md:hidden'
+        onClick={() => setShowMobileMenu(!showMobileMenu)}
+      >
         <FiMenu size={24} className='dark:text-gray-300' />
       </button>
       <div className='hidden md:flex items-center space-x-4 dark:text-gray-300'>
-        <Link to='/products/1'>Erkek</Link>
-        <Link to='/products/2'>Kadın</Link>
-        <Link to='/products/3'>Çocuk</Link>
-        <Link to='/products/4'>Aksesuar</Link>
+        {leftNav.map((item) => (
+          <Link to={item.url}>{item.title}</Link>
+        ))}
         <FiSearch
           ref={domNode}
           size={18}
@@ -86,6 +97,18 @@ const Navbar = () => {
       </div>
       {isOpen && <Cart />}
       {openSearch && <Searchbox />}
+
+      {/* Mobile Menu */}
+      {showMobileMenu && (
+        <div className='absolute top-0 left-0 z-50 bg-white shadow-2xl h-full w-64 flex flex-col py-5 px-10 space-y-4'>
+          <button onClick={() => setShowMobileMenu(!showMobileMenu)}>
+            <FaTimes size={32} />
+          </button>
+          {leftNav.map((item) => (
+            <Link to={item.url}>{item.title}</Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
